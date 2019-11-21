bfb1ce8c9b8e7e0ce9b0cf3a0a0a9e50ce4ec545
Evan Flack <evanjflack@gmail.com>
2019-11-21 13:59:34 -0500

    removed the need for treat_time_var in bacon()

--- R/bacon.R
+++ R/bacon.R
@@ -6,8 +6,6 @@
 #' @param df a data.frame
 #' @param id_var character, name of id variable for units
 #' @param time_var character, name of time variable
-#' @param treat_time_var character, name of variable indicating the time period
-#'  when treatment switched on for unit. Set to NA if unit is never treated.
 #' @param treated_var character, name of treatment variable (binary)
 #' @param outcome_var character, name of outcome variable
 #'
@@ -17,7 +15,6 @@
 #' df_bacon <- bacon(df = bacon::math_reform,
 #'                   id_var = "state",
 #'                   time_var = "class",
-#'                   treat_time_var = "reformyr_math",
 #'                   treated_var = "reform_math",
 #'                   outcome_var = "incearn_ln")
 #' ggplot(df_bacon) +
@@ -25,13 +22,25 @@
 #'   labs(x = "Weight", y = "Estimate", shape = "Type") +
 #'   geom_point()
 #' @export
-bacon <- function(df, id_var = "id", time_var = "time",
-                  treat_time_var = "treat_time", treated_var = "treated",
+bacon <- function(df, id_var = "id",
+                  time_var = "time",
+                  treated_var = "treated",
                   outcome_var = "outcome") {
   # Rename variables
   df <- df %>%
-    rename("id" = id_var, "time" = time_var, "treat_time" = treat_time_var,
-           "treated" = treated_var, "outcome" = outcome_var)
+    rename("id" = id_var, "time" = time_var, "treated" = treated_var,
+           "outcome" = outcome_var)
+
+  df_treat <- df %>%
+    group_by(id) %>%
+    filter(treated == 1) %>%
+    filter(row_number() ==1) %>%
+    select(id, time) %>%
+    rename("treat_time" = "time")
+
+  df <- df %>%
+    merge(df_treat, by = "id", all.x = T) %>%
+    arrange(id, time)
 
   # Set NAS for treat_time to 99999
   df[is.na(df$treat_time), "treat_time"] <- 99999
