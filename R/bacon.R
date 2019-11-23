68b00c62744ac994f8c7e238a15abfb2ee9de8cc
Ed Jee <edjee96@gmail.com>
2019-11-23 17:01:24 -0500

    added donttest to example using ggplot2 as it's not listed as a dependency -> caused RCMD check to fail

--- R/bacon.R
+++ R/bacon.R
@@ -31,10 +31,16 @@
 #'                   data = bacon::castle,
 #'                   id_var = "state",
 #'                   time_var = "year")
+#'
+#' \donttest{
+#'
 #' ggplot(df_bacon) +
 #'   aes(x = weight, y = estimate, shape = factor(type)) +
 #'   labs(x = "Weight", y = "Estimate", shape = "Type") +
 #'   geom_point()
+#'
+#'   }
+#'
 #'
 #' @export
 bacon <- function(formula,
