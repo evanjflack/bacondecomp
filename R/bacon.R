d0cc7bffa7c879acb7aee2889b07c475a1a66727
Evan Flack <evanjflack@gmail.com>
2019-11-23 18:53:07 -0500

    added donttest around first ggplot

--- R/bacon.R
+++ R/bacon.R
@@ -23,6 +23,7 @@
 #'                   time_var = "class")
 #'
 #' \donttest{
+#'
 #' ggplot(df_bacon) +
 #'   aes(x = weight, y = estimate, shape = factor(type)) +
 #'   labs(x = "Weight", y = "Estimate", shape = "Type") +
