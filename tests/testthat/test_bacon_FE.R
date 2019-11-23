fbd297b59ca34804b6908528907c94712a892fda
Ed Jee <edjee96@gmail.com>
2019-11-23 16:53:08 -0500

    added tests

---
+++ tests/testthat/test_bacon_FE.R
@@ -0,0 +1,27 @@
+library(bacon)
+
+# This test calculates the bacon decomposition and then takes the weighted sum
+# of the decomposition which should return the traditional two-way FE estimates
+
+
+# Bacon decomp
+df_bacon <- bacon(l_homicide ~ post,
+                  data = bacon::castle,
+                  id_var = "state",
+                  time_var = "year")
+# Weighted Sum
+two_way_bacon_coef <- sum(df_bacon$estimate * df_bacon$weight)
+
+
+# Two way FE
+two_way_fe <- lm(l_homicide ~ post + factor(state) + factor(year),
+                 data = bacon::castle)
+two_way_fe_coef <- two_way_fe$coefficients["post"]
+names(two_way_fe_coef) <- NULL
+
+
+test_that("Two Way FE recovered", {
+  expect_equal(two_way_bacon_coef, two_way_fe_coef)
+})
+
+
