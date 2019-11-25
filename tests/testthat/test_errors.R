e626928a8568624b647088e8240076cd35271f80
Evan Flack <evanjflack@gmail.com>
2019-11-25 16:52:02 -0500

    added test for unbalanced panel error

---
+++ tests/testthat/test_errors.R
@@ -0,0 +1,19 @@
+library(bacon)
+
+# This test calculates the bacon decomposition and then takes the weighted sum
+# of the decomposition which should return the traditional two-way FE estimates
+
+
+# Bacon decomp
+castle <- bacon::castle
+castle <- castle[-sample(1:nrow(castle), 1), ]
+
+test_that("Unbalanced panel error", {
+  expect_error(bacon(l_homicide ~ post,
+                     data = castle,
+                     id_var = "state",
+                     time_var = "year"), "Unbalanced Panel")
+})
+
+
+
