d3ede41db4876d27fae1f9679c1947d9c97b93a8
Ed Jee <edjee96@gmail.com>
2019-12-08 21:15:29 -0500

    added checks for multivariate return object

--- tests/testthat/test_bacon_FE.R
+++ tests/testthat/test_bacon_FE.R
@@ -3,7 +3,6 @@ library(bacon)
 # This test calculates the bacon decomposition and then takes the weighted sum
 # of the decomposition which should return the traditional two-way FE estimates
 
-
 # Bacon decomp
 df_bacon <- bacon(l_homicide ~ post,
                   data = bacon::castle,
@@ -24,4 +23,20 @@ test_that("Two Way FE recovered", {
   expect_equal(two_way_bacon_coef, two_way_fe_coef)
 })
 
+
+
+##### Multivariate #####
+
+
+df_bacon_multivariate <- bacon(l_homicide ~ post + income + police,
+                               data = bacon::castle,
+                               id_var = "state",
+                               time_var = "year")
+
+
+test_that("Multivariate bacon returns working object", {
+  expect_equal(class(df_bacon_multivariate), "data.frame")
+  expect_true(nrow(df_bacon_multivariate) > 0)
+  expect_true(ncol(ddf_bacon_multivariate) > 0)
+})
 
