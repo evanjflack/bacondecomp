294bb21c6fced914bff9b175065810cb88989fb5
Ed Jee <edjee96@gmail.com>
2019-12-12 17:56:43 -0500

    added variable unpacking tests

---
+++ tests/testthat/test_unpacking.R
@@ -0,0 +1,12 @@
+library(bacon)
+
+
+# Formulas
+variables_unpacked <- unpack_variable_names(l_homicide ~ post + l_pop + l_income)
+
+variables_unpacked
+test_that("Variables Unpacked Correctly", {
+  expect_equal(variables_unpacked$outcome_var, "l_homicide")
+  expect_equal(variables_unpacked$treated_var, "post")
+  expect_equal(variables_unpacked$control_vars, c("l_pop", "l_income"))
+})
