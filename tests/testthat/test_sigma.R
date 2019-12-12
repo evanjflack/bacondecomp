53d4596fb3370b12d6f4ccdc22cc1649ba4f4f34
Evan Flack <evanjflack@gmail.com>
2019-12-11 22:21:42 -0500

    commit

--- tests/testthat/test_sigma.R
+++ tests/testthat/test_sigma.R
@@ -1,7 +1,18 @@
 library(bacon)
 
-data <- bacon::castle
-formula <- l_homicide ~ post + l_pop + l_income
-id_var <- "state"
-time_var <- "year"
+df_test <- data.frame(time = rep(seq(1, 100), 3),
+                      id = c(rep(1, 100), rep(2, 100), rep(3, 100)),
+                      treated = c(rep(0, 100), rep(0, 33), rep(1, 67),
+                                  rep(0, 84), rep(1, 16)),
+                      y = rnorm(300), 
+                      x1 = rcauchy(300))
+
+control_vars <- "x1"
+
+control_formula <- paste(control_vars, collapse = " + ")
+control_formula <- as.formula(paste("treated ~", control_formula))
+
+data <- calculate_ds(df_test, control_formula)
+Sigma <- calculate_Sigma(data)
+one_minus_Sigma <- calculate_one_minus_Sigma(data)
 
